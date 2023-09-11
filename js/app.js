95c2b7703f3462df9c9f05019a4f0ee948f4068e
Alexander <kanashkin_ad@mail.ru>
2023-09-11 22:45:49 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-const baseFunc = () => {
-    
+const dropDownMenu = () => {
+    const triggerBtn = document.querySelector('.dropdown__trigger')
+    const menu = document.querySelector('.dropdown__menu')
+
+    triggerBtn.addEventListener('click', () => {
+        menu.classList.toggle('active')
+    })
 }
+
+dropDownMenu()
+
+const collapse = () => {
+    const triggerBtn = document.querySelectorAll('.collapse__block-trigger')
+
+    triggerBtn.forEach(item => {
+        item.addEventListener('click', () => {
+            item.parentNode.parentNode.classList.toggle('active')
+        })
+    })
+}
+
+collapse()
